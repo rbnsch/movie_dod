1b23f2bcb87cd0c97d6a3322c7061f5f2081cd52
rbnsch <robinschmidt0@gmx.de>
2021-11-28 17:14:03 +0200

    Changes for merge

--- APIRequests.py
+++ APIRequests.py
@@ -1,7 +1,6 @@
 import requests
 import json
-from StorageWriter import WriteStorage
-'''
+
 url = 'https://imdb-api.com/en/API/Company/'
 
 payload = {}
@@ -15,21 +14,14 @@ while True:
     requestURL = url + api_key + "/" + company_key + "&start=" + str(start)
     print(requestURL)
     response = requests.request("GET", requestURL, headers=headers, data = payload)
-    print(response.status_code)
+
     start += 50
 
     if response.status_code == 200:
         data = response.json()
         if data["errorMessage"] == "":
-            movies = []
             print(data["name"])
             for movie in data["items"]:
                 print(movie["title"])
-                movies.append(movie["title"])
         if start >= 1000 or not data["items"]:
             break;
-'''
-movies = [ ("First", 2), ("Secodn", 3), ("Last", 3)]
-print(movies)
-writestorage = WriteStorage("data.csv")
-writestorage.writeContent(movies)
